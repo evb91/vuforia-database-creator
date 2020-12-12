4fdcd56793f28bfa575d44f77d5e63e801ccb00f
Eleanor Bird <eleanorvbird@gmail.com>
2020-12-12 23:04:36 +0000

    Takes database name and license name as parameters

--- login.py
+++ login.py
@@ -4,7 +4,8 @@ import os
 import uuid
 import time
 
-def create_database(vuforia_username, vuforia_password):
+
+def create_database(vuforia_username, vuforia_password, license_name, database_name):
 
     driver = webdriver.Safari()
     driver.implicitly_wait(10)
@@ -17,8 +18,6 @@ def create_database(vuforia_username, vuforia_password):
     elem = driver.find_element_by_id("get-development-key")
     elem.click()
     elem = driver.find_element_by_id("license-name")
-    random = uuid.uuid4().hex
-    license_name = 'generated_license_name_' + str(random)
     elem.send_keys(license_name)
     elem = driver.find_element_by_id("agree-terms-checkbox")
     elem.submit()
@@ -29,12 +28,11 @@ def create_database(vuforia_username, vuforia_password):
     time.sleep(5)
     elem.click()
     elem = driver.find_element_by_id("database-name")
-    database_name = 'database_name_' + str(random)
     elem.send_keys(database_name)
     elem = driver.find_element_by_id("cloud-radio-btn")
     elem.click()
-    license_name_dashes = license_name.replace('_', '-')
-    license_dropdown_id = 'cloud-license-' + license_name_dashes
+    license_name_dashes = license_name.replace("_", "-")
+    license_dropdown_id = "cloud-license-" + license_name_dashes
     elem = driver.find_element_by_id(license_dropdown_id)
     time.sleep(5)
     elem.click()
@@ -44,17 +42,28 @@ def create_database(vuforia_username, vuforia_password):
     elem = driver.find_element_by_xpath(database_name_xpath)
     elem.click()
     time.sleep(5)
-    elem = driver.find_element_by_link_text('Database Access Keys')
+    elem = driver.find_element_by_link_text("Database Access Keys")
     elem.click()
-    elem = driver.find_element_by_class_name('client-access-key')
+    elem = driver.find_element_by_class_name("client-access-key")
     client_access_key = elem.text
-    elem = driver.find_element_by_class_name('client-secret-key')
+    elem = driver.find_element_by_class_name("client-secret-key")
     client_secret_key = elem.text
-    elem = driver.find_element_by_class_name('server-access-key')
+    elem = driver.find_element_by_class_name("server-access-key")
     server_access_key = elem.text
-    elem = driver.find_element_by_class_name('server-secret-key')
+    elem = driver.find_element_by_class_name("server-secret-key")
     server_secret_key = elem.text
     driver.close()
 
-create_database(vuforia_username=os.environ['VUFORIA_EMAIL_ADDRESS'],
-vuforia_password=os.environ['VUFORIA_PASSWORD'])
+
+random = uuid.uuid4().hex
+vuforia_username = os.environ["VUFORIA_EMAIL_ADDRESS"]
+vuforia_password = os.environ["VUFORIA_PASSWORD"]
+license_name = "generated_license_name_" + str(random)
+database_name = "database_name_" + str(random)
+
+create_database(
+    vuforia_username=vuforia_username,
+    vuforia_password=vuforia_password,
+    license_name=license_name,
+    database_name=database_name,
+)
